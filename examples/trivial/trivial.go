1b7be1b41869857d55f4f031ce7673b6842903df
Niek Sanders <niek.sanders@gmail.com>
2014-10-24 00:33:46 -0700

    Output now goes to stderr.

--- examples/trivial/trivial.go
+++ examples/trivial/trivial.go
@@ -1,29 +1,36 @@
+/*
+ * A trivial example consuming from Kinesis using the KCL daemon.
+ *
+ * Just echos messages it sees to stderr.
+ */
 package main
 
 import (
 	"fmt"
 	"kinesis"
+	"os"
 )
 
 type EchoConsumer struct {
 }
 
 func (ec *EchoConsumer) Init(shardId string) error {
-	fmt.Printf("init: %s\n", shardId)
+	fmt.Fprintf(os.Stderr, "init: %s\n", shardId)
 	return nil
 }
 
 func (ec *EchoConsumer) ProcessRecords(records []*kinesis.KclRecord,
 	checkpointer *kinesis.Checkpointer) error {
 	for i := range records {
-		fmt.Printf("process: %s\n", records[i].DataB64)
+		fmt.Fprintf(os.Stderr, "process: %s\n", records[i].DataB64)
 	}
+	checkpointer.CheckpointAll()
 	return nil
 }
 
 func (ec *EchoConsumer) Shutdown(shutdownType kinesis.ShutdownType,
 	checkpointer *kinesis.Checkpointer) error {
-	fmt.Printf("shutdown: %s\n", shutdownType)
+	fmt.Fprintf(os.Stderr, "shutdown: %s\n", shutdownType)
 	if shutdownType == kinesis.GracefulShutdown {
 		checkpointer.CheckpointAll()
 	}
